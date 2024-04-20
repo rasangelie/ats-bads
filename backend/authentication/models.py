bc78fa0c471730d79e795b835d9c12b2e8f8bbca
Angelie <angeliebadar1@gmail.com>
2024-04-20 13:11:20 +0800

    chore: delete unnecessary fields in User model

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,6 +1,8 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-import uuid
+
 
 # Create your models here.
 class User(AbstractUser):
@@ -9,15 +11,14 @@ class User(AbstractUser):
         MANAGER = 2, 'Manager'
         CONTRIBUTOR = 3, 'Contributor'
     
-    role = models.IntegerField(choices=UserRoleChoices.choices, default=UserRoleChoices.MANAGER)
+    # role = models.IntegerField(choices=UserRoleChoices.choices, default=UserRoleChoices.MANAGER)
+    role = models.IntegerField(
+        choices=[(choice.value, choice.name) for choice in UserRoleChoices],
+        default=UserRoleChoices.MANAGER,
+    )
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    first_name = models.CharField(max_length=100)
-    middle_initial = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email_address = models.EmailField()
-    created_at = models.TimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    
 
     def __str__(self):
         return self.first_name
